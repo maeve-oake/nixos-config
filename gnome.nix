32651771e2bd1c314a2a01cc35272356169e1924
maeve <maeve@oa.ke>
2024-11-30 20:01:15 +0000

    add some keybinds

--- gnome.nix
+++ gnome.nix
@@ -9,7 +9,7 @@
 		extraGSettingsOverrides = ''
 			[org.gnome.shell]
 			favorite-apps=['microsoft-edge.desktop', 'discord.desktop', 'org.telegram.desktop.desktop', 'code.desktop', 'org.gnome.Console.desktop', 'org.gnome.Nautilus.desktop']
-			welcome-dialog-last-shown-version='100'
+		        # welcome-dialog-last-shown-version='100' # never show tour
 			
 			[org.gnome.desktop.interface]
 			color-scheme='prefer-dark'
@@ -19,12 +19,61 @@
 			[org.gnome.mutter]
 			dynamic-workspaces=true
 			experimental-features=['scale-monitor-framebuffer']
+
+			# keybinds
+
+			[org.gnome.desktop.wm.keybindings]
+			close=['<Shift><Super>q']
+			maximize=['<Shift><Super>Up']
+			move-to-monitor-down=@as []
+			move-to-monitor-left=@as []
+			move-to-monitor-right=@as []
+			move-to-monitor-up=@as []
+			move-to-workspace-1=['<Shift><Super>1']
+			move-to-workspace-2=['<Shift><Super>2']
+			move-to-workspace-3=['<Shift><Super>3']
+			move-to-workspace-4=['<Shift><Super>4']
+			move-to-workspace-5=['<Shift><Super>5']
+			move-to-workspace-6=['<Shift><Super>6']
+			move-to-workspace-7=['<Shift><Super>7']
+			move-to-workspace-8=['<Shift><Super>8']
+			move-to-workspace-9=['<Shift><Super>9']
+			move-to-workspace-left=['<Shift><Control><Super>Left']
+			move-to-workspace-right=['<Shift><Control><Super>Right']
+			switch-input-source=@as []
+			switch-input-source-backward=@as []
+			switch-to-workspace-1=['<Super>1']
+			switch-to-workspace-2=['<Super>2']
+			switch-to-workspace-3=['<Super>3']
+			switch-to-workspace-4=['<Super>4']
+			switch-to-workspace-5=['<Super>5']
+			switch-to-workspace-6=['<Super>6']
+			switch-to-workspace-7=['<Super>7']
+			switch-to-workspace-8=['<Super>8']
+			switch-to-workspace-9=['<Super>9']
+			toggle-fullscreen=['<Super>f']
+			unmaximize=['<Shift><Super>Down']
+
+			[org.gnome.mutter.keybindings]
+			toggle-tiled-left=['<Shift><Super>Left']
+			toggle-tiled-right=['<Shift><Super>Right']
+
+			[org.gnome.shell.keybindings]
+			focus-active-notification=@as []
+			switch-to-application-1=@as []
+			switch-to-application-2=@as []
+			switch-to-application-3=@as []
+			switch-to-application-4=@as []
+			toggle-application-view=@as []
+			toggle-message-tray=@as []
+
 		'';
 
 		extraGSettingsOverridePackages = [
 			pkgs.gnome.gnome-shell
 			pkgs.gsettings-desktop-schemas
 			pkgs.gnome.mutter
+			pkgs.gnome.gnome-settings-daemon
 		];
 	};
 }
