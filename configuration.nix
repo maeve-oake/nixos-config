05ebf0c5fc98848ca47b35492e681614178fa7d3
maeve <maeve@oa.ke>
2024-12-01 10:57:32 +0000

    add dotnet pkgs

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,9 @@
     vscode
     neovim
     gh
+    dotnet-sdk
+    dotnet-runtime
+    dotnet-aspnetcore
 
     # apps
     microsoft-edge
@@ -75,19 +78,6 @@
     zoxide
     fzf
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
