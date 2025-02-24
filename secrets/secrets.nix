b4b98104b1461e1926dc6a1dfb6e0bf1781aa751
maeve <maeve@oa.ke>
2025-02-24 09:09:59 +0000

    add aluminium hostkey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   maeve = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDvkX/XN4U6idAnpWO9JbFpKxJFsvGzfmSCCFKIMmpv";
   replika = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOM0F0SG96eXo7K5In/cFi3/R8JyU7uGrmir8s/qDf87";
-  hosts = [ replika ];
+  aluminium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjsH0Zu/aVH+uDjzALotADjozJp0yfrf4OAIVJFXud3";
+  hosts = [ replika aluminium ];
 in
 {
   "mynah-smb.age".publicKeys = [ maeve ] ++ hosts;
