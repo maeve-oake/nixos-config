d783536ce6dc17710c44271e3130fa921df556c8
maeve <maeve@oa.ke>
2025-03-03 13:47:42 +0000

    fix edge aad sync

--- hosts/aluminium/configuration.nix
+++ hosts/aluminium/configuration.nix
@@ -30,6 +30,37 @@
     teams-for-linux
   ];
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      # overlay to fix microsoft edge sync w/ aad accounts
+      # https://github.com/NixOS/nixpkgs/issues/343401#issuecomment-2692747174
+      microsoft-edge = prev.microsoft-edge.overrideAttrs
+        (old: (
+          let
+            deps = [
+              # extra dependencies for libmip_core/libmip_protection_sdk
+              final.libsecret
+              final.glib
+              final.stdenv.cc.cc.lib
+              final.libxml2
+              final.libuuid
+            ];
+          in
+          {
+            rpath = old.rpath + ":" + lib.makeLibraryPath deps + ":" + lib.makeSearchPathOutput "lib" "lib64" deps;
+            binpath = old.binpath + ":" + lib.makeBinPath deps;
+
+            installPhase = (
+              builtins.replaceStrings [ "--prefix XDG_" ] [
+                ''--set SSL_CERT_FILE "${final.cacert}/etc/ssl/certs/ca-bundle.crt" --prefix XDG_''
+              ]
+                old.installPhase
+            );
+          }
+        ));
+    })
+  ];
+
   # Do not remove
   system.stateVersion = "24.05";
 }
