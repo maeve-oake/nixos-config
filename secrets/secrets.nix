7806eeb0e37ef2aadef3b0b7987dd66a0c83b7fc
maeve <maeve@oa.ke>
2025-05-09 23:18:40 +0100

    secrets: add elster hostkey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,8 @@ let
   maeve = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDvkX/XN4U6idAnpWO9JbFpKxJFsvGzfmSCCFKIMmpv";
   replika = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOM0F0SG96eXo7K5In/cFi3/R8JyU7uGrmir8s/qDf87";
   aluminium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjsH0Zu/aVH+uDjzALotADjozJp0yfrf4OAIVJFXud3";
-  hosts = [ replika aluminium ];
+  elster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzeE24EUgkih3TLH7hn5ZsHcACIsk6VAVl3ve2SaSgF";
+  hosts = [ replika aluminium elster ];
 in
 {
   "maeve-password.age".publicKeys = [ maeve ] ++ hosts;
