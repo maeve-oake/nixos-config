8bdeed579e9dc2e986c85a1ca7eb8c999a455ef0
maeve <maeve@oa.ke>
2025-05-12 20:24:10 +0100

    elster things

--- hosts/elster/configuration.nix
+++ hosts/elster/configuration.nix
@@ -10,11 +10,12 @@
     ./hardware-configuration.nix
     #../../common/x86_64-linux/i3
     ../../common/x86_64-linux/gnome
-    ../../common/x86_64-linux/secureboot.nix
+    # ../../common/x86_64-linux/secureboot.nix
     #../../common/x86_64-linux/samba.nix
   ];
 
   # boot
+  boot.loader.systemd-boot.enable = true;
   boot.loader.timeout = 0;
   boot.kernelPackages = pkgs.linuxPackages_6_14;
 
@@ -32,6 +33,20 @@
   # fingerprint & login
   services.fprintd.enable = true;
   security.polkit.enable = true;
+
+  # # graphics
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  # hardware.graphics = {
+  #   enable = true;
+  # };
+  # hardware.nvidia = {
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  #   modesetting.enable = true;
+  #   powerManagement.enable = false;
+  #   powerManagement.finegrained = false;
+  #   open = false;
+  #   nvidiaSettings = true;
+  # };
 
   # packages
   programs.steam.enable = true;
