e096c5f3ebee9c406c2c8f5f52f1aa48fd3ac59d
maeve <maeve@oa.ke>
2025-05-14 21:00:15 +0100

    add ncd! i love anya

--- common/fish.nix
+++ common/fish.nix
@@ -15,6 +15,20 @@
         nix-shell -p $argv --run fish
       end
 
+      function ncd --description "cd into nix store path of a package"
+        set package_name $argv[1]
+        if test -z "$package_name"
+          echo "Usage: ncd <package-name>"
+          return 1
+        end
+        set store_path (NIXPKGS_ALLOW_UNFREE=1 nix path-info nixpkgs#$package_name --impure | string trim)
+        if test -z "$store_path"
+          echo "Could not find nix store path for package: $package_name"
+          return 1
+        end
+        cd $store_path
+      end
+
       zoxide init fish --cmd cd | source
     '';
   };
