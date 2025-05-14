2318e018621c9ffb403d904c498a6ef8ca66c974
maeve <maeve@oa.ke>
2025-05-14 14:49:47 +0100

    rework flake & add README

---
+++ README.md
@@ -0,0 +1,33 @@
+# maeve's nixos config
+
+- `common` - shared configuration for all machines
+
+    - `aarch64-darwin` common configs for MacOS computers
+
+    - `x86_64-linux` common configs for x86 Linux computers
+        
+        - `gnome`, `i3`, `hyprland` - WM & DE configs
+
+            - `pkgs` - WM & DE specific packages
+
+- `hosts` - hardware specific configuration
+
+    - `aluminium` - Framework 13 work laptop
+
+    - `elster` - rack-mounted gaming PC
+
+    - `replika` - Framework 13 personal laptop
+
+    - `stainless` - 2021 MacBook Pro work laptop
+
+- `pkgs` - non arch-specific packges
+
+- `secrets` - age encrypted secrets
+
+## flake layout
+
+yeag
+
+## install instructions
+
+soon
--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
     nix-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
 
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
+
     nix-homebrew = {
       url = "github:zhaofengli/nix-homebrew";
       inputs.nixpkgs.follows = "nix-darwin";
@@ -23,70 +25,16 @@
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-flatpak.url = "github:gmodena/nix-flatpak";
   };
   outputs =
     inputs:
     let
-      commonModules = [
-        ./common
-        inputs.agenix.nixosModules.default
-      ];
-
-      x86_64-linux_commonModules = commonModules ++ [
-        ./common/x86_64-linux
-        inputs.lanzaboote.nixosModules.lanzaboote
-        inputs.nix-index-database.nixosModules.nix-index
-        inputs.nix-flatpak.nixosModules.nix-flatpak
-      ];
-
-      x86_64-linux_unstableNixpkgs = {
-        unstable = import inputs.nix-unstable {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-      };
-
-      aarch64-darwin_commonModules = commonModules ++ [
-        ./common/aarch64-darwin
-        inputs.nix-homebrew.darwinModules.nix-homebrew
-      ];
+      inherit (import ./functions.nix inputs) mkNixos mkDarwin mkMerge;
     in
-    {
-      nixosConfigurations = {
-        aluminium = inputs.nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = x86_64-linux_commonModules ++ [
-            ./hosts/aluminium/configuration.nix
-          ];
-          specialArgs = x86_64-linux_unstableNixpkgs;
-        };
-
-        replika = inputs.nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = x86_64-linux_commonModules ++ [
-            ./hosts/replika/configuration.nix
-          ];
-          specialArgs = x86_64-linux_unstableNixpkgs;
-        };
-
-        elster = inputs.nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = x86_64-linux_commonModules ++ [
-            ./hosts/elster/configuration.nix
-          ];
-          specialArgs = x86_64-linux_unstableNixpkgs;
-        };
-      };
-
-      darwinConfigurations = {
-        stainless = inputs.nix-darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          modules = aarch64-darwin_commonModules ++ [
-            ./hosts/stainless/configuration.nix
-          ];
-        };
-      };
-    };
+    mkMerge [
+      (mkNixos "aluminium" inputs.nixpkgs [ ])
+      (mkNixos "replika" inputs.nixpkgs [ ])
+      (mkNixos "elster" inputs.nixpkgs [ ])
+      (mkDarwin "stainless" inputs.nix-darwin [ ])
+    ];
 }
