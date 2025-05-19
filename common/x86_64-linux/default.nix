e5e8542092f1e9c29738c34c9d3a670a49de214c
Anna Oake <anna@oa.ke>
2025-05-19 17:02:36 +0200

    ci: use GH runners, add darwin, switch to PAT, build PRs; darwin: update flake url (#13)
    * ci: use GH runners, add darwin
    
    * darwin: update flake url
    
    * ci: use PAT instead of GITHUB_TOKEN
    
    * ci: fix space issue

--- common/x86_64-linux/default.nix
+++ common/x86_64-linux/default.nix
@@ -24,16 +24,6 @@ in
     inputs.nix-flatpak.nixosModules.nix-flatpak
   ];
 
-  # cache
-  nix.settings.extra-substituters = [
-    "https://attic.oa.ke/nixos"
-  ];
-  nix.settings.extra-trusted-public-keys = [
-    "nixos:qbhh36l2BlhnNhXnU0I2XHOzIT3mzwxKfs86C4am5aY="
-  ];
-  age.secrets.attic-netrc.file = (inputs.self + /secrets/attic-netrc.age);
-  nix.settings.netrc-file = config.age.secrets.attic-netrc.path;
-
   # boot
   boot.loader.timeout = 0;
 
