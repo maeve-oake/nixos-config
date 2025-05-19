b3070764e15b3b2c2661f556b3a4d84fd3a147ed
Anna Oake <anna@oa.ke>
2025-05-19 13:59:01 +0200

    ci: use GH runners, add darwin

--- common/default.nix
+++ common/default.nix
@@ -1,4 +1,4 @@
-{ inputs, hostname, ... }:
+{ inputs, hostname, config, ... }:
 {
   # common configuration for all architectures
   # please see subdirectories for arch-specific configuration
@@ -19,4 +19,14 @@
     "nix-command"
     "flakes"
   ];
+
+  # cache
+  nix.settings.extra-substituters = [
+    "https://attic.oa.ke/nixos"
+  ];
+  nix.settings.extra-trusted-public-keys = [
+    "nixos:qbhh36l2BlhnNhXnU0I2XHOzIT3mzwxKfs86C4am5aY="
+  ];
+  age.secrets.attic-netrc.file = (inputs.self + /secrets/attic-netrc.age);
+  nix.settings.netrc-file = config.age.secrets.attic-netrc.path;
 }
