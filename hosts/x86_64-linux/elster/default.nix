e0375f015387772256c1243c0d774a914f083a7b
maeve ~ <maeve@oa.ke>
2025-05-22 17:26:46 +0100

    elster: secureboot

--- hosts/x86_64-linux/elster/default.nix
+++ hosts/x86_64-linux/elster/default.nix
@@ -11,13 +11,13 @@
     ./hardware-configuration.nix
     # (inputs.self + /common/x86_64-linux/i3)
     (inputs.self + /common/x86_64-linux/gnome)
-    # (inputs.self + /common/x86_64-linux/secureboot.nix)
+    (inputs.self + /common/x86_64-linux/secureboot.nix)
     # (inputs.self + /common/x86_64-linux/samba.nix)
   ];
 
   # boot
-  boot.loader.systemd-boot.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_6_14;
+  boot.kernelModules = [ "firewire_ohci" ];
 
   # power & sleep
   swapDevices = [
