1966680007b8f1379a99b82fbd94e58c353bacaf
maeve <maeve@oa.ke>
2025-05-23 10:00:18 +0100

    darwin: primary system user

--- common/aarch64-darwin/user.nix
+++ common/aarch64-darwin/user.nix
@@ -1,10 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   users.knownUsers = [ "maeve" ];
   users.users.maeve = {
     shell = pkgs.fish;
     uid = 501;
   };
+  config.system.primaryUser = "maeve";
 
   security.pam.enableSudoTouchIdAuth = true;
 }
