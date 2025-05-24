06c26e1bb1f79d3e2bab2c9d36b6ffa1ac6b5210
maeve <maeve@oa.ke>
2025-05-24 15:35:33 +0100

    secrets: add aluminium host key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,7 @@ let
   replika = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOM0F0SG96eXo7K5In/cFi3/R8JyU7uGrmir8s/qDf87";
   aluminium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjsH0Zu/aVH+uDjzALotADjozJp0yfrf4OAIVJFXud3";
   elster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzeE24EUgkih3TLH7hn5ZsHcACIsk6VAVl3ve2SaSgF";
+  stainless = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+t14PSPA87rn3D/V71YhTkQSpy6xhXrQ7JCXI/AiJO";
   hosts = [ replika aluminium elster ];
   all = [ maeve ] ++ hosts;
 in
@@ -10,7 +11,7 @@ in
   "maeve-password.age".publicKeys = all;
   "mynah-vault.age".publicKeys = [ maeve replika ];
   "mynah-smb.age".publicKeys = [ maeve replika ];
-  "attic-netrc.age".publicKeys = all;
+  "attic-netrc.age".publicKeys = all ++ [ stainless ];
 
   # wifi 
   "wifi-home.age".publicKeys = all; 
