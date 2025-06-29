91158830845f8b0857d3ddaeb2bb472b461f5e02
maeve <maeve@oa.ke>
2025-06-29 14:57:41 +0200

    change to use new repo

--- pkgs/plymouth-1975-theme/default.nix
+++ pkgs/plymouth-1975-theme/default.nix
@@ -9,11 +9,8 @@ stdenvNoCC.mkDerivation {
   version = "1.0.0";
 
   src = fetchgit {
-    url = "https://github.com/maeve-oake/cfg";
-    sparseCheckout = [
-      "plymouth/1975"
-    ];
-    sha256 = "sha256-zgTVX07PdfGcVmdrPEcOe/l7e6dAevcZtmDyUJfR6VM=";
+    url = "https://github.com/maeve-oake/plymouth-1975-theme";
+    sha256 = "sha256-OT4GwqDwQgF0kVMKPN0kSQoRHAuTAcvA1nh4PTGBaK8=";
   };
 
   dontBuild = true;
@@ -22,14 +19,14 @@ stdenvNoCC.mkDerivation {
     runHook preInstall
 
     mkdir -p $out/share/plymouth/themes/1975
-    cp plymouth/1975/* $out/share/plymouth/themes/1975
+    cp * $out/share/plymouth/themes/1975
     find $out/share/plymouth/themes/ -name \*.plymouth -exec sed -i "s@\/usr\/@$out\/@" {} \;
 
     runHook postInstall
   '';
 
   meta = {
-    description = "Plymouth theme with The 1975 graphics";
+    description = "The 1975 Plymouth boot splash";
     platforms = lib.platforms.linux;
   };
 }
