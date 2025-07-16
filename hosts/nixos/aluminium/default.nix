67dd97c75dbe47ce0cc75bc5ffda42dba767e4a9
anna-oake <anna@oa.ke>
2025-07-16 21:49:51 +0200

    aluminium: configure streamdeck

--- hosts/nixos/aluminium/default.nix
+++ hosts/nixos/aluminium/default.nix
@@ -37,6 +37,81 @@
     teams-for-linux
   ];
 
+  # streamdeck
+  services.streamdeck = {
+    enable = true;
+    config = {
+      defaultBrightness = 100;
+      defaultPage = "home";
+      renderFont = "${pkgs.roboto}/share/fonts/truetype/Roboto-Regular.ttf";
+      pages = {
+        home = {
+          keys = {
+            "0" = {
+              display.text = {
+                text = "zero";
+                border = 20;
+              };
+            };
+            "1" = {
+              display.text = {
+                text = "weblinks";
+                image = "/home/maeve/Documents/streamdeck/icons/maeve_cat.jpg";
+              };
+              actions = [
+                {
+                  page = {
+                    name = "weblinks";
+                  };
+                }
+              ];
+            };
+            "2" = {
+              display.text = {
+                text = "edge";
+              };
+              actions = [
+                {
+                  page = {
+                    name = "weblinks";
+                  };
+                }
+              ];
+            };
+            "3" = {
+              display.image = {
+                url = "https://les.bi/images/maeve.png";
+              };
+              actions = [
+                {
+                  page = {
+                    name = "weblinks";
+                  };
+                }
+              ];
+            };
+          };
+        };
+        weblinks = {
+          keys = {
+            "1" = {
+              display.text = {
+                text = "back";
+              };
+              actions = [
+                {
+                  page = {
+                    name = "home";
+                  };
+                }
+              ];
+            };
+          };
+        };
+      };
+    };
+  };
+
   # Do not remove
   system.stateVersion = "24.05";
 }
