28e5ddb4c11992fa38d50ef373079ea31a99be3c
maeve ~ <maeve@oa.ke>
2025-08-28 23:56:28 +0100

    samba: refactor mounts (#64)
    Co-authored-by: Anna Oake <anna@oa.ke>

--- common/nixos/samba.nix
+++ common/nixos/samba.nix
@@ -6,40 +6,42 @@
   ...
 }:
 let
-  username = config.me.username;
-  mynahOptions = [
-    "x-systemd.automount"
-    "noauto"
-    "x-systemd.idle-timeout=5s"
-    "x-systemd.device-timeout=5s"
-    "x-systemd.mount-timeout=5s"
-    "soft"
-    "uid=${toString config.users.users.${username}.uid}"
-    "gid=${toString config.users.groups.users.gid}"
-  ];
+  mkMounts =
+    server: credentials: mounts:
+    lib.attrsets.mapAttrs (name: share: {
+      device = "//${server}/${share}";
+      fsType = "cifs";
+      options = [
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=5s"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.mount-timeout=5s"
+        "soft"
+        "uid=${toString config.users.users.${config.me.username}.uid}"
+        "gid=${toString config.users.groups.users.gid}"
+        "credentials=${credentials}"
+      ];
+    }) mounts;
 in
 {
   options.maeve.samba.enable = lib.mkEnableOption "mounting of Samba shares";
 
   config = lib.mkIf config.maeve.samba.enable {
-    age.secrets.mynah-smb.file = (inputs.self + /secrets/mynah-smb.age);
+    age.secrets.maeve-mynah-smb.file = (inputs.self + /secrets/maeve-mynah-smb.age);
+    age.secrets.anna-mynah-smb.file = (inputs.self + /secrets/anna-mynah-smb.age);
     environment.systemPackages = with pkgs; [
       cifs-utils
       gocryptfs
     ];
 
-    fileSystems = {
-      "/mnt/mynah/maeve" = {
-        device = "//anya-nas.maeve/maeve";
-        fsType = "cifs";
-        options = mynahOptions ++ [ "credentials=${config.age.secrets.mynah-smb.path}" ];
-      };
-
-      "/mnt/mynah/oake" = {
-        device = "//anya-nas.maeve/oake";
-        fsType = "cifs";
-        options = mynahOptions ++ [ "credentials=${config.age.secrets.mynah-smb.path}" ];
-      };
-    };
+    fileSystems =
+      (mkMounts "share.lan.ci" config.age.secrets.maeve-mynah-smb.path {
+        "/mnt/mynah/maeve" = "maeve";
+      })
+      // (mkMounts "share.lan.al" config.age.secrets.anna-mynah-smb.path {
+        "/mnt/anna-mynah/maeve" = "maeve";
+        "/mnt/anna-mynah/oake" = "oake";
+      });
   };
 }
