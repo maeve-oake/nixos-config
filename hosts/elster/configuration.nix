175b77db7f09c40cd7e970eced4919a1cbf5f8b0
Anna Oake <anna@oa.ke>
2025-09-06 16:00:13 +0200

    elster: pin nvidia driver

--- hosts/elster/configuration.nix
+++ hosts/elster/configuration.nix
@@ -2,7 +2,6 @@
   inputs,
   config,
   pkgs,
-  unstable,
   ...
 }:
 {
@@ -45,6 +44,15 @@
     powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
+
+    # TODO: figure this out once they fix it
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.65.06";
+      sha256_64bit = "sha256-BLEIZ69YXnZc+/3POe1fS9ESN1vrqwFy6qGHxqpQJP8=";
+      openSha256 = "sha256-BKe6LQ1ZSrHUOSoV6UCksUE0+TIa0WcCHZv4lagfIgA=";
+      settingsSha256 = "sha256-9PWmj9qG/Ms8Ol5vLQD3Dlhuw4iaFtVHNC0hSyMCU24=";
+      usePersistenced = false;
+    };
   };
 
   # camera
