4d9a6111bd38bbef1b14227b274237a21a553542
Anna Oake <anna@oa.ke>
2025-09-11 16:21:10 +0200

    lxc-builder: support aarch64-linux via gratis (#80)
    * lxc-builder: enable remote builders
    
    * profiles.server: make ghostty terminfo great again
    
    * lxc-builder: make deploy comfy again (for anya)
    
    * ci: don't build aarch64-linux on GH

--- hosts/lxc-builder/configuration.nix
+++ hosts/lxc-builder/configuration.nix
@@ -6,8 +6,9 @@
 {
   imports = [
     inputs.self.nixosModules.default
-    ./buildbot.nix
     ./attic-watch.nix
+    ./buildbot.nix
+    ./remote-builders.nix
   ];
 
   profiles.server.enable = true;
@@ -22,6 +23,11 @@
   users.users.root.openssh.authorizedKeys.keys = [
     # anya uses lxc-builder as a remote builder for x86_64-linux stuff
     config.me.wifeKey
+  ];
+
+  deploy.sshKeys = [
+    config.me.deployKey
+    config.me.wifeKey # anya manages lxc-builder so needs to deploy to it
   ];
 
   system.stateVersion = "25.11";
