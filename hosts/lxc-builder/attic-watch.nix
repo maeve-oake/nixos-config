8100b6fcafb59415c8e499ffd2e73cd6f77e59b6
Anna Oake <anna@oa.ke>
2025-11-06 14:58:06 +0100

    attic: split-cache magic

--- hosts/lxc-builder/attic-watch.nix
+++ hosts/lxc-builder/attic-watch.nix
@@ -3,28 +3,33 @@
   pkgs,
   ...
 }:
-{
-  age.secrets."lxc-builder/attic-auth-token" = { };
-
-  systemd.services.attic-watch-store = {
+let
+  mkAtticService = name: extraArgs: {
     wantedBy = [ "multi-user.target" ];
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
-    environment.HOME = "/var/lib/attic-watch-store";
+    environment.HOME = "/var/lib/attic-${name}-watch-store";
     serviceConfig = {
       DynamicUser = true;
       MemoryMax = "15%";
-      LoadCredential = "attic-auth-token:${config.age.secrets."lxc-builder/attic-auth-token".path}";
-      StateDirectory = "attic-watch-store";
+      LoadCredential = "attic-${name}-auth-token:${
+        config.age.secrets."lxc-builder/attic-auth-token".path
+      }";
+      StateDirectory = "attic-${name}-watch-store";
       Restart = "always";
     };
     path = [ pkgs.attic-client ];
     script = ''
       set -eux -o pipefail
-      ATTIC_TOKEN=$(< $CREDENTIALS_DIRECTORY/attic-auth-token)
-      attic login attic https://attic.oa.ke $ATTIC_TOKEN
-      attic use nixos
-      exec attic watch-store attic:nixos
+      ATTIC_TOKEN=$(< $CREDENTIALS_DIRECTORY/attic-${name}-auth-token)
+      attic login attic-${name} https://attic-${name}.oa.ke $ATTIC_TOKEN
+      exec attic watch-store attic-${name}:nixos ${extraArgs}
     '';
   };
+in
+{
+  age.secrets."lxc-builder/attic-auth-token" = { };
+
+  systemd.services.attic-buyan-watch-store = mkAtticService "buyan" "--ignore-upstream-cache-filter";
+  systemd.services.attic-kitezh-watch-store = mkAtticService "kitezh" "";
 }
