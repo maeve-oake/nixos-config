a6705bfc76d32775a3ef5532675ee83087597dd6
Anna Oake <anna@oa.ke>
2025-12-05 00:33:29 +0100

    lxc-lancache: add second account

--- hosts/lxc-lancache/configuration.nix
+++ hosts/lxc-lancache/configuration.nix
@@ -10,7 +10,10 @@
 
   profiles.server.enable = true;
 
-  age.secrets."lxc-lancache/steam-token-maeve" = { };
+  age.secrets = {
+    "lxc-lancache/steam-token-maeve" = { };
+    "lxc-lancache/steam-token-maeve-2" = { };
+  };
 
   services.lancache = {
     enable = true;
@@ -18,13 +21,23 @@
 
     prefill.steam = {
       enable = true;
-      accounts.maeve = {
-        tokenFile = config.age.secrets."lxc-lancache/steam-token-maeve".path;
-        prefillAll = true;
-        systems = [
-          "windows"
-          "linux"
-        ];
+      accounts = {
+        maeve = {
+          tokenFile = config.age.secrets."lxc-lancache/steam-token-maeve".path;
+          prefillAll = true;
+          systems = [
+            "windows"
+            "linux"
+          ];
+        };
+        maeve-2 = {
+          tokenFile = config.age.secrets."lxc-lancache/steam-token-maeve-2".path;
+          prefillAll = true;
+          systems = [
+            "windows"
+            "linux"
+          ];
+        };
       };
     };
   };
