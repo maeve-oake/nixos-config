19c6f9db9b488f3a9d5a7384b0f58dcad4e88af5
Anna Oake <anna@oa.ke>
2026-01-24 17:14:57 +0100

    lxc-docker: init (#112)
    * flake: update nix-things
    
    * lxc-docker: init
    
    * rekey: lxc-docker
    
    ---------
    
    Co-authored-by: maeve <maeve@oa.ke>

--- modules/nixos/profiles/server/default.nix
+++ modules/nixos/profiles/server/default.nix
@@ -17,5 +17,14 @@
     environment.systemPackages = with pkgs; [
       ghostty.terminfo
     ];
+
+    # mynah host driver
+    lxc.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.119.02";
+      sha256_64bit = "sha256-gCD139PuiK7no4mQ0MPSr+VHUemhcLqerdfqZwE47Nc=";
+      openSha256 = "sha256-l3IQDoopOt0n0+Ig+Ee3AOcFCGJXhbH1Q1nh1TEAHTE=";
+      useSettings = false;
+      usePersistenced = false;
+    };
   };
 }
