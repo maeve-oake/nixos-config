2a877c23ee8a6f59eeec6dea365200204b3bd67f
Anna Oake <anna@oa.ke>
2026-02-15 15:42:11 +0000

    elster: temporary nvidia upgrade to fix build failure

--- hosts/elster/configuration.nix
+++ hosts/elster/configuration.nix
@@ -42,15 +42,19 @@
 
   # graphics
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.graphics = {
-    enable = true;
-  };
+  hardware.graphics.enable = true;
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
     open = true;
-    nvidiaSettings = true;
+    # TODO: might wanna remove when https://github.com/NixOS/nixpkgs/issues/489947 is fixed
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.126.09";
+      sha256_64bit = "sha256-TKxT5I+K3/Zh1HyHiO0kBZokjJ/YCYzq/QiKSYmG7CY=";
+      sha256_aarch64 = "sha256-c5PEKxEv1vCkmOHSozEnuCG+WLdXDcn41ViaUWiNpK0=";
+      openSha256 = "sha256-ychsaurbQ2KNFr/SAprKI2tlvAigoKoFU1H7+SaxSrY=";
+      settingsSha256 = "sha256-4SfCWp3swUp+x+4cuIZ7SA5H7/NoizqgPJ6S9fm90fA=";
+      persistencedSha256 = "sha256-J1UwS0o/fxz45gIbH9uaKxARW+x4uOU1scvAO4rHU5Y=";
+    };
   };
 
   # camera
