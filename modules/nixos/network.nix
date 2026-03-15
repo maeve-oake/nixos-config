8cc8600477e6b94b8a39ae12a4f5fe28d9296e9d
Anna Oake <anna@oa.ke>
2026-03-15 15:43:58 +0100

    nixos: use unstable netbird module

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -1,8 +1,17 @@
 {
+  inputs,
   unstable,
   ...
 }:
 {
+  disabledModules = [
+    "services/networking/netbird.nix"
+  ];
+
+  imports = [
+    "${inputs.nix-unstable}/nixos/modules/services/networking/netbird.nix"
+  ];
+
   services.netbird = {
     package = unstable.netbird;
     ui.package = unstable.netbird-ui;
