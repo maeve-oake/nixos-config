31f2c3524418123122a42237afbe1e4a0e04d5bb
maeve <maeve@oa.ke>
2026-03-24 15:16:12 +0000

    blush: init

---
+++ hosts/blush/darwin-configuration.nix
@@ -0,0 +1,16 @@
+{
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.self.darwinModules.default
+  ];
+
+  profiles.workstation.enable = true;
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # Do not remove
+  system.stateVersion = 6;
+}
