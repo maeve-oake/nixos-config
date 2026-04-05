25041a389ad874d74b6999795831b258ea481662
maeve <maeve@oa.ke>
2026-04-05 16:02:12 +0200

    update ssh key

--- modules/common/me.nix
+++ modules/common/me.nix
@@ -2,7 +2,7 @@
   config.me = {
     username = "maeve";
     email = "maeve@oa.ke";
-    sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDvkX/XN4U6idAnpWO9JbFpKxJFsvGzfmSCCFKIMmpv maeve@oa.ke";
+    sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZNKMNZpuqfniJkCK+//Cz+hI3WH2l78Txp9I/maeve";
     wifeKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHf6UCNeXSN8WAZ9cXh8jz61+jbP+ts+inct/CCjcN/o anna@oa.ke";
     deployKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIELcdN5rDjO2x59GCkBUGevuyc+w9mwX6YC8Zh15zIVK";
     lanDomain = "lan.ci";
