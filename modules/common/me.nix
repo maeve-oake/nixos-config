55967502f9dc8a0687bc5605e22077b6882a6efd
maeve ~ <maeve@oa.ke>
2026-04-10 12:10:32 +0200

    update wifekey (#123)

--- modules/common/me.nix
+++ modules/common/me.nix
@@ -3,7 +3,7 @@
     username = "maeve";
     email = "maeve@oa.ke";
     sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZNKMNZpuqfniJkCK+//Cz+hI3WH2l78Txp9I/maeve";
-    wifeKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHf6UCNeXSN8WAZ9cXh8jz61+jbP+ts+inct/CCjcN/o anna@oa.ke";
+    wifeKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2jBAbYXwrY13EBAVyk5QaG3xOR0d9IfbpkgP8/anna";
     deployKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIELcdN5rDjO2x59GCkBUGevuyc+w9mwX6YC8Zh15zIVK";
     lanDomain = "lan.ci";
   };
