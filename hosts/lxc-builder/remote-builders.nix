4583cae195ddb5c23c6f59189365f0dd6eed5829
Anna Oake <anna@oa.ke>
2026-04-13 15:33:43 +0200

    lxc-builder: enable netbird

--- hosts/lxc-builder/remote-builders.nix
+++ hosts/lxc-builder/remote-builders.nix
@@ -18,4 +18,15 @@
     config.nixpkgs.hostPlatform.system # local build
     "aarch64-linux" # build on gratis
   ];
+
+  age.secrets.netbird-homelab = {
+    owner = "netbird";
+    group = "netbird";
+  };
+
+  services.netbird.simple = {
+    enable = true;
+    managementUrl = "https://net.oa.ke";
+    setupKeyFile = config.age.secrets.netbird-homelab.path;
+  };
 }
