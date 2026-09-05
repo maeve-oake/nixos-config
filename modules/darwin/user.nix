942f92702fd64ec4f7c8b10f2b40648712a9bd2b
maeve <maeve@oa.ke>
2026-09-05 17:35:41 +0200

    formatting fix

--- modules/darwin/user.nix
+++ modules/darwin/user.nix
@@ -4,16 +4,16 @@
   ...
 }:
 {
-    users.knownUsers = [ config.me.username ];
-    users.users.${config.me.username} = {
-      uid = 501;
-      home = "/Users/${config.me.username}";
-      openssh.authorizedKeys.keys = [
-        config.me.sshKey
-      ];
-      shell = pkgs.fish;
-    };
-    system.primaryUser = config.me.username;
-    
-    security.pam.services.sudo_local.touchIdAuth = true;
+  users.knownUsers = [ config.me.username ];
+  users.users.${config.me.username} = {
+    uid = 501;
+    home = "/Users/${config.me.username}";
+    openssh.authorizedKeys.keys = [
+      config.me.sshKey
+    ];
+    shell = pkgs.fish;
+  };
+  system.primaryUser = config.me.username;
+
+  security.pam.services.sudo_local.touchIdAuth = true;
 }
